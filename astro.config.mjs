a611877b771ab0ec7e44032a3683ae3e12fe5104
Andrea Aguirre Rendón <60911269+4nd12030@users.noreply.github.com>
2024-12-26 21:59:27 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://4nd12030.github.io',
     integrations: [
         mdx(),
         sitemap(),
